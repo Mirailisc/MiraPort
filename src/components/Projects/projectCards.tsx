b2cb1f3b8df815a6b6af6c01cea83e975b1b7332
Mirailisc <mirailisclm@gmail.com>
2021-09-08 19:01:00 +0700

    Fix Projects Bug

--- src/components/Projects/projectCards.tsx
+++ src/components/Projects/projectCards.tsx
@@ -1,25 +1,20 @@
-import MiraBot from '../../image/MiraBot.png'
-
 export const projectCards = [
     {
         name: 'MiraBot',
         key: 'card1',
         detail: 'An utility bot for MiraCommunity Discord Server',
         url: 'https://github.com/Mirailisc/mirabot-discord',
-        image: MiraBot,
     },
     {
         name: 'MiraPort',
         key: 'card2',
         detail: "My Portfolio website which shows some of my information",
         url: 'https://github.com/Mirailisc/MiraPort',
-        image: MiraBot,
     },
     {
         name: 'MiraSchool',
         key: 'card3',
         detail: "A website to manage my school's work schedule, class schedule, and Google Meet attendance.",
         url: 'https://github.com/Mirailisc/MiraSchool',
-        image: MiraBot,
     },
 ]
