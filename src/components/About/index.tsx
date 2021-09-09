f0f7db3f8773b59fd954da75ef3722aac6d81c3b
Mirailisc <mirailisclm@gmail.com>
2021-09-09 09:00:13 +0700

    edit about content

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -11,14 +11,16 @@ const About: React.FC = (): JSX.Element => {
         <div className={styles.content}>
           <img className={styles.image} src={Image1} alt="Image_1" data-aos="zoom-in" />
           <div className={styles.text}>
-            <h3 data-aos="fade-down">PHUBORDIN POOLNAI</h3>
+            <h3 data-aos="fade-down">Hi, I’m Mark. A human who just started the journey at the Tech industry.</h3>
             <p data-aos="fade-up">
-              I am a 16 years old junior high school student, working as an Intern Web Developer at Datability.
-              I started my journey by watching technology grow with the world. <span> ( actually, by playing "Minecraft" lol ) </span>
-              I started learning to code by learning Python, making a task management
-              website for myself and my friends.
+              I am a 16 years old junior high school student, working as an Intern Web Developer at Datability. I
+              started my journey by watching technology grow with the world.{' '}
+              <span> ( actually, by playing "Minecraft" lol ) </span>I started learning to code by learning Python,
+              making a task management website for myself and my friends.
             </p>
-            <p data-aos="fade-up">And, I love to do something fun and challenge for improve myself and entertain others</p>
+            <p data-aos="fade-up">
+              And, I love to do something fun and challenge for improve myself and entertain others
+            </p>
             <div className={styles.fact} data-aos="fade-up">
               <h4>FUN FACT</h4>
               <p>I love Murasaki Shion and Nanashi Mumei</p>
